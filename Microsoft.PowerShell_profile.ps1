b69668d66ea4a966ebb174eb9915921ea2c7b601
SodaCris <wang-jiaxun@outlook.com>
2022-05-31 18:17:10 +0800

    change theme and icon

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
 . $PSScriptRoot/myconfig.ps1
+
+function w {
+    where_command_go $args
+}
+
+$Env:PATH += ";C:\Program Files\Chez Scheme 9.5.8\bin\ta6nt"
+Import-Module -Name Terminal-Icons
+
+oh-my-posh init pwsh --config C:\Users\18463\scoop\apps\oh-my-posh\current\themes/jandedobbeleer.omp.json | Invoke-Expression
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pwshprofile
 
 My Powershell scripts.
+
+参考：https://docs.microsoft.com/en-us/windows/terminal/tutorials/custom-prompt-setup
